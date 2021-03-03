610a50e7233335c05a6ad50377352bc843d225a9
PJ <pjboulder@gmail.com>
2021-03-03 01:51:47 +0000

    updating py functions and adding bash script

--- weather.py
+++ weather.py
@@ -1,1 +1,39 @@
-# pythong to come
+# python to come
+import os
+import psycopg2
+from psycopg2.extras import DictCursor
+
+def db_connect():
+  db_name = os.environ['db_name']
+  db_user = os.environ['db_user']
+  db_host = os.environ['db_host']
+  db_credentials = os.environ['db_creds']
+ 
+  conn_string = "dbname='" + str(db_name) + "' user='" + str(db_user) + "' host='" + str(db_host) + "' password='" + str(db_credentials) + "'"
+
+  try:
+    conn = psycopg2.connect(str(conn_string))
+    conn.autocommit = True
+  except:
+    print("Unable to connect to the database")
+
+  cur = conn.cursor(cursor_factory=DictCursor)
+  return cur
+
+
+
+dwh_cur = db_connect()
+def select_data():
+	test_sql = "select * from weather.emdat_america_disasters LIMIT 5"
+
+	dwh_cur.execute(test_sql)
+	rows = dwh_cur.fetchall()
+	for row in rows:
+	   print(row['Dis No'])
+
+def insert_data():
+	test_sql = "INSERT INTO weather.test VALUES ('blah','blah2')"
+	dwh_cur.execute(test_sql)
+
+# insert_data()
+
---
+++ weather_sync.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+/usr/bin/python3.7 /home/theraceblogger/weather-db-example/weather.py

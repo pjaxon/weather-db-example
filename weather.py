a78c8b0a947e1413411a69a922ed00254f345fcb
Chuck Schultz <theraceblogger@comcast.net>
2021-03-02 19:08:42 -0700

    add noaa get data

--- weather.py
+++ weather.py
@@ -37,3 +37,14 @@ def insert_data():
 
 # insert_data()
 
+def get_noaa_data():
+  try:
+    noaa_token = os.environ['noaa_token']
+    header = {'token': noaa_token}
+    url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:80455&startdate=2020-01-01&enddate=2020-01-31&limit=10"
+    r = request.get(url, headers=header)
+    print(r.content)
+  except:
+    print('You fucked something up!')
+
+get_noaa_data()

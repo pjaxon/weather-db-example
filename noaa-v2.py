7b6a5c83415d579f1d7a7a2b3c0128bc5d4e8b29
Chuck Schultz <theraceblogger@comcast.net>
2021-03-16 17:42:49 -0600

    test

--- noaa-v2.py
+++ noaa-v2.py
@@ -47,8 +47,8 @@ def load_data(url, off_set=1):
 # Function gets NOAA data and loads into database
 def get_noaa(station):
     start, end = get_station_params(station)
-    start_dt = datetime.date(int(start))
-    end_dt = datetime.date(int(end))
+    start_dt = datetime.strptime(start)
+    end_dt = datetime.strptime(end)
     num_years = end_dt.year - start_dt.year + 1
 
     for year in range(num_years):

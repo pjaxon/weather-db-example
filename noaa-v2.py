b1ee894c045e227cfa6a8cfc45ac251aeaba2942
Chuck Schultz <theraceblogger@comcast.net>
2021-03-16 15:31:44 -0600

    test

--- noaa-v2.py
+++ noaa-v2.py
@@ -24,7 +24,7 @@ def get_station_params(station):
     #max_query = f"SELECT sr.station_jsonb ->> 'maxdate' FROM weather.stations_raw sr WHERE sr.station_id = '{station}'"
     cur.execute(min_query)
     start = cur.fetchall()
-    return start
+    return start[0]
 
 
 # Function gets NOAA data and loads into database

2bb9dc98226ba880d4ca4b60e67751d4c2cdad8b
Chuck Schultz <theraceblogger@comcast.net>
2021-03-16 13:21:50 -0600

    add file for regular noaa data

--- noaa-v2.py
+++ noaa-v2.py
@@ -1,4 +1,4 @@
-## This script gets 118,487 stations data from NOAA, and stores it in weather.stations_raw
+## This script gets data from NOAA, and stores it in weather.raw
 import os
 import psycopg2
 from psycopg2.extras import DictCursor
@@ -8,33 +8,38 @@ import json
 import time
 
 # Set variables
-noaa_token = os.environ['noaa_token']
 header = {'token': noaa_token}
-base_url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations"
+base_url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data"
 dataset_id = "?datasetid=GHCND"
-limit = "&limit=1000"
+data_types = ""
+locations = ""
+stations = ""
+start_date = "&startdate=2020-01-01"
+end_date = "&enddate=2020-01-03"
+limit = "&limit=10"
 
-# Function gets NOAA station data (118,487 entries - 1000 at a time) and loads into database
+# Function gets NOAA data and loads into database
 def get_noaa_stations(entry_number = 1):
     try:
         time.sleep(.5)
         offset = "&offset=" + str(entry_number)
-        url = base_url + dataset_id + limit + offset
+        url = base_url + dataset_id + data_types + locations + stations + start_date + end_date + limit + offset
         print('Starting entry num: ' + str(entry_number) + ', ' + url)
         r = requests.get(url, headers=header)
         j = r.json()
 
         for result in j['results']:
             try:
-                insert_sql = "INSERT INTO weather.stations_raw (station_id, station_jsonb) VALUES (%s,%s) ON CONFLICT (station_id) DO UPDATE SET station_jsonb = %s"
-                cur.execute(insert_sql, (result['id'], json.dumps(result, indent=4, sort_keys=True), json.dumps(result, indent=4, sort_keys=True))) 
+                print(result)
+                # insert_sql = "INSERT INTO weather.stations_raw (station_id, station_jsonb) VALUES (%s,%s) ON CONFLICT (station_id) DO UPDATE SET station_jsonb = %s"
+                # cur.execute(insert_sql, (result['id'], json.dumps(result, indent=4, sort_keys=True), json.dumps(result, indent=4, sort_keys=True))) 
             except:
                 print ('could not iterate through results')
         
-        entry_number += 1000       
-        if (entry_number < j['metadata']['resultset']['count']): 
-            #print('get_noaa_stations looping')
-            get_noaa_stations(entry_number)
+        # entry_number += 1000       
+        # if (entry_number < j['metadata']['resultset']['count']): 
+        #     #print('get_noaa_stations looping')
+        #     get_noaa_stations(entry_number)
                 
     except:
         print('Function failed')

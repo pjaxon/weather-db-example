362e023b8007e826f636336a83ec8cd6d660b8f5
Chuck Schultz <theraceblogger@comcast.net>
2021-03-19 12:00:36 -0600

    ex test

--- explore_data.py
+++ explore_data.py
@@ -37,6 +37,4 @@ query = 'SELECT column_name FROM information_schema.columns WHERE table_schema =
 #query = 'SELECT * FROM weather.stations_raw LIMIT 10'
 cur.execute(query)
 results = cur.fetchall()
-for result in results:
-    print(result)
-
+print(results[0], results[1])
